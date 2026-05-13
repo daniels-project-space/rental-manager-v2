2ffd92295bd8d2412d72bd608066def497105d03
Daniel Mabro <daniel.mabro@gmail.com>
2026-05-13 00:28:18 +0000

    fix: bust Turbopack cache to pick up NEXT_PUBLIC_CONVEX_URL=exciting-lion-29 prod env

--- src/lib/convex.ts
+++ src/lib/convex.ts
@@ -1,5 +1,6 @@
 import { ConvexReactClient } from "convex/react";
 
+// NEXT_PUBLIC_CONVEX_URL must point to the prod deployment (exciting-lion-29)
 export const convex = new ConvexReactClient(
   process.env.NEXT_PUBLIC_CONVEX_URL as string
 );
